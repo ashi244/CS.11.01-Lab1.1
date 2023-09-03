75e4f12d1713979d3e80063fc065ec28d2474c61
angelashi <angelas2026@student.cis.edu.hk>
2023-09-03 15:25:38 +0800

    Added in input and output

--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your age: ");
+        String stringAge = scanner.nextLine();
+        int age = Integer.parseInt(stringAge);
+        int pages = 100 - age;
+        System.out.print("\n" + age + "-year olds should read at least " + pages + " pages before giving up on a book");
     }
 }
