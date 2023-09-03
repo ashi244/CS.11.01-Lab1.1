911f0c5f444b0a0b7c9688ac3d21d0262afd25c2
angelashi <angelas2026@student.cis.edu.hk>
2023-09-03 15:22:27 +0800

    Added in input and output

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,13 @@
+
+import java.util.Scanner;
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your age: ");
+        String stringAge = scanner.nextLine();
+        int age = Integer.parseInt(stringAge);
+        int dateableAge = 7 + age/2;
+        System.out.print("\n" + age + "-year olds should date someone who is at least " + dateableAge + " years old");
     }
 }
